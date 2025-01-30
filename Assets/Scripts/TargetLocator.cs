7d87de6765c81e8b7995aa556b3cd96e109e4129
mertsekerunity <mertseker9393@gmail.com>
2025-01-30 17:01:22 +0100

    target distance is measured.
    target distance is measured.

--- Assets/Scripts/TargetLocator.cs
+++ Assets/Scripts/TargetLocator.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class TargetLocator : MonoBehaviour
 {
     [SerializeField] Transform weapon;
+    [SerializeField] ParticleSystem projectilePrefab;
+    [SerializeField] float range = 25f;
+
     Transform target;
 
     // Start is called before the first frame update
@@ -16,11 +19,50 @@ public class TargetLocator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        FindClosestEnemy();
         AimWeapon();
     }
 
     void AimWeapon()
     {
+        float targetDistance = Vector3.Distance(target.position, transform.position);
+
         weapon.LookAt(target);
+
+        if (targetDistance < range)
+        {
+            Attack(true);
+        }
+        else
+        {
+            Attack(false);
+        }
+    }
+
+    void FindClosestEnemy()
+    {
+        EnemyController[] enemies = FindObjectsOfType<EnemyController>();
+
+        Transform closestTarget = null;
+
+        float maxDistance = Mathf.Infinity;
+
+        foreach(EnemyController enemy in enemies)
+        {
+            float targetDistance = Vector3.Distance(enemy.transform.position, transform.position);
+
+            if (targetDistance < maxDistance)
+            {
+                closestTarget = enemy.transform;
+                maxDistance = targetDistance;
+            }
+        }
+        target = closestTarget;
+    }
+
+    void Attack(bool isActive)
+    {
+        var emissionModule = projectilePrefab.emission;
+        emissionModule.enabled = isActive;
     }
 }
